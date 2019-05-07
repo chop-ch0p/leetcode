9a73dbf4ac531890ad492367d1a087e2b8b39488
nzt <difgrt@gmail.com>
2019-05-07 22:13:23 +0900

    update 13.roman-to-integer.cpp

--- 13.roman-to-integer.cpp
+++ 13.roman-to-integer.cpp
@@ -5,39 +5,44 @@ private:
     map<Roman, int> table;
 
 public:
-    Solution() {
-        table.emplace(Roman('I', 'V'), 4);
-        table.emplace(Roman('I', 'X'), 9);
-        table.emplace(Roman('X', 'L'), 40);
-        table.emplace(Roman('X', 'C'), 90);
-        table.emplace(Roman('C', 'D'), 400);
-        table.emplace(Roman('C', 'M'), 900);
-        table.emplace(Roman('I', 'A'), 1);
-        table.emplace(Roman('V', 'A'), 5);
-        table.emplace(Roman('X', 'A'), 10);
-        table.emplace(Roman('L', 'A'), 50);
-        table.emplace(Roman('C', 'A'), 100);
-        table.emplace(Roman('D', 'A'), 500);
-        table.emplace(Roman('M', 'A'), 1000);
-        
-    }
+    
     int romanToInt(string s) {
         int result = 0;
         int len = s.length();
 
         for (int i = 0; i < len; ++i) {
-            char c = s.at(i);
-            char n = (i+1 < len) ? s.at(i+1) : '0';
-            map<Roman, int>::iterator it = table.find(Roman(c, n));
-
-            if (it != table.end()) {
-                result += it->second;
-                ++i; 
-                continue;
-            } 
-
-            it = table.find(Roman(c, 'A'));
-            result += it->second;
+            char c = s[i];
+            char n = s[i+1];
+            
+            switch(c) {
+                case 'I':
+                    if (n == 'V') { result += 4; ++i; }
+                    else if ( n == 'X') { result += 9; ++i; }
+                    else { result++; }
+                    break;
+                case 'V':
+                    result += 5;
+                    break;
+                case 'X':
+                    if (n == 'L') { result += 40; ++i; }
+                    else if ( n == 'C') { result += 90; ++i; }
+                    else { result += 10; }
+                    break;
+                case 'L':
+                    result += 50;
+                    break;
+                case 'C':
+                    if (n == 'D') { result += 400; ++i; }
+                    else if ( n == 'M') { result += 900; ++i; }
+                    else { result += 100; }
+                    break;
+                case 'D':
+                    result += 500;
+                    break;
+                case 'M':
+                    result += 1000;
+                    break;
+            }
         }
 
         return result;
