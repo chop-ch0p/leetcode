8edcbbb58579970b6c27d4f2d83e8e6cdd3b5408
nzt <difgrt@gmail.com>
2019-05-09 20:39:21 +0900

    update 58.length-of-last-word.cpp

--- 58.length-of-last-word.cpp
+++ 58.length-of-last-word.cpp
@@ -3,9 +3,11 @@ public:
     int lengthOfLastWord(string s) {
         int count = 0;
         int result = 0;
+        const char* chr = s.c_str();
+        int len = s.length();
 
-        for (auto i : s) {
-            if (i == ' ') {
+        for (int i = 0 ; i < len; ++i) {
+            if (chr[i] == ' ') {
                 if (count > 0)
                     result = count;
                 count = 0;
