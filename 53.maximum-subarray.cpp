fc9b08970a3bce1aad66b4498923d8a43fc5963a
nzt <difgrt@gmail.com>
2019-05-09 20:35:48 +0900

    update 53.maximum-subarray.cpp

---
+++ 53.maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max = nums[0];
+        int sum = 0;
+        for (auto i : nums) {
+            if (sum + i < i) sum = i;
+            else
+                sum += i;
+
+            if (max < sum) max = sum;
+        }
+
+        return max;
+    }
+};
