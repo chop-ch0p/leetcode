3cb487972a7e7e7039d29ca0c0b548fb1f63d2f2
nzt <difgrt@gmail.com>
2019-05-13 23:43:08 +0900

    update 111.minimum-depth-of-binary-tree.cpp

--- 111.minimum-depth-of-binary-tree.cpp
+++ 111.minimum-depth-of-binary-tree.cpp
@@ -9,17 +9,19 @@
  */
 class Solution {
 private:
-    int result;
-public:
-    int minDepth(TreeNode* root) {
+    int minDepth_recursive(TreeNode* root) {
         if (root == nullptr) return 0;
 
-        int hl = minDepth(root->left);
-        int hr = minDepth(root->right);
+        int hl = minDepth_recursive(root->left);
+        int hr = minDepth_recursive(root->right);
 
         if (hl == 0 || hr == 0)
             return max(hl, hr) + 1;
         else
             return min(hl, hr) + 1;
+    }
+public:
+    int minDepth(TreeNode* root) {
+       return minDepth_recursive(root);
     }
 };
